22867b25b527404a6faf6f2715c8cb8268b68ac7
Sankalp Singha <sankalpsingha@gmail.com>
2013-06-25 20:54:47 +0530

    Added infinite scroll in the view too

--- protected/config/main.php
+++ protected/config/main.php
@@ -76,7 +76,7 @@ return array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-				'<controller>' => '<controller>',
+				//'<controller>' => '<controller>',
 				'<slug:[\w\_]+>'=>'user/view',
 			),
 		),
