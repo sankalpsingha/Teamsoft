e44a59a258d74109860196936b4d9b9b7fe88502
Sankalp Singha <sankalpsingha@gmail.com>
2013-06-25 23:51:42 +0530

    Added few things for the view

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -37,6 +37,7 @@ This is the property of the CampusPlugin Team.
 										array('label'=>'Complaint', 'url'=>array('/complaint/create'),'icon'=>'icon-warning-sign'),
 										array('label'=>'Create Module', 'url'=>array('/module/create'),'icon'=>'icon-tag'),
 										array('label'=>'Blog', 'url'=>array('/blog/'),'icon'=>'icon-bullhorn'),
+										array('label'=>'CAD/GALLERY', 'url'=>array('/blog/'),'icon'=>'icon-picture'),
 										array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 										array('label'=>'Logout ('.User::model()->findByPk(Yii::app()->user->id)->name.')', 'url'=>array('/site/logout'), 'icon'=>'icon-signout', 'visible'=>!Yii::app()->user->isGuest)
 									)
